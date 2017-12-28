b47629472cb3853bf08232a7be90c5151c394b16
ClothoQ <30002933+ClothoQ@users.noreply.github.com>
2017-12-28 23:54:57 +0000

    UPD

--- src/bittrexAPI.java
+++ src/bittrexAPI.java
@@ -1,15 +1,9 @@
 package bittrex;
 
 import java.security.InvalidKeyException;
-import java.security.MessageDigest;
+
 import java.security.NoSuchAlgorithmException;
 import java.security.SignatureException;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.Formatter;
-
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
 
 import java.net.*;
 import java.io.*;
@@ -22,62 +16,45 @@ public class bittrexAPI {
 	static String apiSecretKEY          = null;
 
 	public static enum Bittrex {
-		PUBLIC, MARKET, ACCOUNT
+		PUBLIC, ACCOUNT
 	}
 
-	public static String BittrexRequest(String API, Bittrex options) {
+	public static String BittrexRequest(String Link, Bittrex options) {
 
-		String hashedHeader2 = null;
-		String Link          = API;
 		String inputLine     = null;
 		String bittrexData   = null;
-		URL url;
 
 		switch (options) {
 		case PUBLIC:
 			try {
-				url = new URL(Link);
-				URLConnection Conn = url.openConnection();
-				BufferedReader Data2 = new BufferedReader(new InputStreamReader(Conn.getInputStream()));
-
-				while ((inputLine = Data2.readLine()) != null) {
+				URLConnection conn = new URL(Link).openConnection();
+    
+				while ((inputLine = new BufferedReader(new InputStreamReader(conn.getInputStream())).readLine()) != null) {
 					bittrexData = inputLine;
 				}
-
-				Data2.close();
-
+				
 				return bittrexData;
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 			break;
 		case ACCOUNT:
-			try {
-				hashedHeader2 = HmacSha512.ENCRYPT(Link, apiSecretKEY);
-				url = new URL(Link);
-				URLConnection conn = url.openConnection();
+			try {		
+				URLConnection conn = new URL(Link).openConnection();
 
 				conn.setConnectTimeout(5000);
 				conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-				conn.setRequestProperty("apisign", hashedHeader2);
-
-				InputStream response = conn.getInputStream();
-
-				BufferedReader Data = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-
-				while ((inputLine = Data.readLine()) != null) {
+				conn.setRequestProperty("apisign", HmacSha512.ENCRYPT(Link, apiSecretKEY));
+				
+				while ((inputLine = new BufferedReader(new InputStreamReader(conn.getInputStream())).readLine()) != null) {
 					bittrexData = inputLine;
 				}
 
-				Data.close();
-
 				return bittrexData;
-
 			} catch (IOException | InvalidKeyException | SignatureException | NoSuchAlgorithmException e) {
 				e.printStackTrace();
 			}
 			break;
-
 		}
 		return "101";
 
@@ -134,19 +111,52 @@ public class bittrexAPI {
 	}
 
 	public static String getBalance(String Currency) {
-		return BittrexRequest(
-				API_URL + "account/getbalance?apikey=" + apiKEY + "&nonce=" + getUnixTime() + "&currency=" + Currency,
-				Bittrex.ACCOUNT);
+		return BittrexRequest(API_URL + "account/getbalance?apikey=" + apiKEY + "&nonce=" + getUnixTime() + "&currency=" + Currency,Bittrex.ACCOUNT);
 	}
 
 	public static String getDepositAddress(String Currency) {
-		return BittrexRequest(API_URL + "account/getdepositaddress?apikey=" + apiKEY + "&nonce=" + getUnixTime()
-				+ "&currency=" + Currency, Bittrex.ACCOUNT);
+		return BittrexRequest(API_URL + "account/getdepositaddress?apikey=" + apiKEY + "&nonce=" + getUnixTime()+ "&currency=" + Currency, Bittrex.ACCOUNT);
+	}
+	
+	public static String getOrderHistory(String Currency) {
+		return BittrexRequest(API_URL + "account/getorderhistory?apikey=" + apiKEY + "&nonce=" + getUnixTime(), Bittrex.ACCOUNT);
+	}
+	
+	public static String getOrder(String uuid) {
+		return BittrexRequest(API_URL + "account/getorder?apikey=" + apiKEY + "&nonce=" + getUnixTime() + "&uuid=" + uuid, Bittrex.ACCOUNT);
+	}
+	
+	public static String getWithdrawalHistory(String Currency) {
+		return BittrexRequest(API_URL + "account/getwithdrawalhistory?apikey=" + apiKEY + "&nonce=" + getUnixTime() + "&currency=" + Currency, Bittrex.ACCOUNT);
+	}
+	
+	public static String getDepositHistory(String Currency) {
+		return BittrexRequest(API_URL + "account/getdeposithistory?apikey=" + apiKEY + "&nonce=" + getUnixTime() + "&currency=" + Currency, Bittrex.ACCOUNT);
 	}
 
 	public static String Withdraw(String Currency, String quantity, String address) {
 		return BittrexRequest(API_URL + "account/withdraw?apikey=" + apiKEY + "&nonce=" + getUnixTime() + "&currency="
 				+ Currency + "&quantity=" + quantity + "&address=" + address, Bittrex.ACCOUNT);
 	}
-
+	
+	// MARKET
+	
+	public static String buyLimit(String Currency, String quantity,String rate) {
+		return BittrexRequest(API_URL + "market/buylimit?apikey=" + apiKEY + "&nonce=" + getUnixTime() + "&market" + Currency +
+				"&quantity" + quantity + "&rate" + rate, Bittrex.ACCOUNT);
+	}
+	
+	public static String sellLimit(String Currency, String quantity,String rate) {
+		return BittrexRequest(API_URL + "market/selllimit?apikey=" + apiKEY + "&nonce=" + getUnixTime() + "&market" + Currency +
+				"&quantity" + quantity + "&rate" + rate, Bittrex.ACCOUNT);
+	}
+	
+	public static String cancelOrder(String uuid) {
+		return BittrexRequest(API_URL + "market/cancel?apikey=" + apiKEY + "&nonce=" + getUnixTime() + "&uuid=" + uuid, Bittrex.ACCOUNT);
+	}
+	
+	public static String getOpenOrders(String Currency) {
+		return BittrexRequest(API_URL + "market/cancel?apikey=" + apiKEY + "&nonce=" + getUnixTime() + "&market=" + Currency, Bittrex.ACCOUNT);
+	}
+	
 }
